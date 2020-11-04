619afc36480ff8b45baf25d5cf98b5e6e1039ba0
LYL232 <3183246942@qq.com>
2020-11-04 21:04:28 +0800

    fix bug: using shanghai timezone

--- backend/src/main/java/pers/lyl232/jakgd/service/BaseService.java
+++ backend/src/main/java/pers/lyl232/jakgd/service/BaseService.java
@@ -2,6 +2,7 @@ package pers.lyl232.jakgd.service;
 
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -14,21 +15,21 @@ public class BaseService {
             DateTimeFormatter.ofPattern("yyyy-MM", Locale.CHINA);
 
     /**
-     * 获取当前北京时间精确到秒的时间字符串
+     * 获取当前上海时间精确到秒的时间字符串
      *
      * @return 字符串
      */
     public String getFormatNowDateStringBySecond() {
-        return secondFormatter.format(ZonedDateTime.now());
+        return secondFormatter.format(ZonedDateTime.now(ZoneId.of("Asia/Shanghai")));
     }
 
     /**
-     * 获取当前北京时间精确到月的时间字符串
+     * 获取当前上海时间精确到月的时间字符串
      *
      * @return 字符串
      */
     public String getFormatNowDateStringByMonth() {
-        return monthFormatter.format(ZonedDateTime.now());
+        return monthFormatter.format(ZonedDateTime.now(ZoneId.of("Asia/Shanghai")));
     }
 
 }
